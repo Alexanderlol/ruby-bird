ab6aeb89516553b182e64654c078888e12ccdc7e
Alex Afeltra <afeltra.alex@gmail.com>
2017-06-20 17:01:32 -0400

    background and foreground anim

--- game.rb
+++ game.rb
@@ -1,4 +1,28 @@
 require 'gosu'
 
-window = Gosu::Window.new(800, 600, false)
+class GameWindow < Gosu::Window
+	def initialize(*args)
+		super
+		@images = {
+			background: Gosu::Image.new(self, 'images/background.png', false),
+		  foreground: Gosu::Image.new(self, 'images/foreground.png', true),
+		}
+		@scroll_x = 0
+	end
+
+	def update
+		@scroll_x += 3
+		if @scroll_x > @images[:foreground].width
+			@scroll_x = 0
+		end
+	end
+
+	def draw
+		@images[:background].draw(0, 0, 0)
+		@images[:foreground].draw(-@scroll_x, 0, 0)
+		@images[:foreground].draw(-@scroll_x + @images[:foreground].width, 0, 0)
+	end
+end
+
+window = GameWindow.new(320, 480, false)
 window.show
